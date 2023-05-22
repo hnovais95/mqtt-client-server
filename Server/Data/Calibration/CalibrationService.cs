cf40016bc9a0c46c0d9a4fa1fbd6ce6cc78b6a1c
Heitor Novais <hnovais95@gmail.com>
2023-05-22 19:47:40 -0300

    set dotnet 5

--- Server/Data/Calibration/CalibrationService.cs
+++ Server/Data/Calibration/CalibrationService.cs
@@ -19,42 +19,11 @@ namespace Server.Data
 
 		public async Task<IEnumerable<Double>> GetPredictions(List<CalibrationParamsDTO> calibrationParams)
 		{
-            //var requestBody = new GetPredictionsRequestDTO { Records = calibrationParams };
-            //         using HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/invocations", requestBody);
-            //         response.EnsureSuccessStatusCode();
-            //var responseBody = await response.Content.ReadFromJsonAsync<GetPredictionsResponseDTO>();
-            //return responseBody.Predictions;
-
-
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    // Fazer uma requisição GET
-                    var requestBody = new GetPredictionsRequestDTO { Records = calibrationParams };
-                    HttpResponseMessage response = await client.PostAsJsonAsync("https://model-mock.herokuapp.com/invocations", requestBody);
-
-                    // Verificar se a requisição foi bem-sucedida
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Ler o conteúdo da resposta
-                        Console.WriteLine("A requisição deu sucesso.");
-                        var responseBody = await response.Content.ReadFromJsonAsync<GetPredictionsResponseDTO>();
-                        return responseBody.Predictions;
-                    }
-                    else
-                    {
-                        Console.WriteLine("A requisição falhou. Código de status: " + response.StatusCode);
-                        return new List<Double>();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Ocorreu um erro: " + ex.Message);
-                    return new List<Double>();
-                }
-            }
-
+			var requestBody = new GetPredictionsRequestDTO { Records = calibrationParams };
+            using HttpResponseMessage response = await _httpClient.PostAsJsonAsync("/invocations", requestBody);
+            response.EnsureSuccessStatusCode();
+			var responseBody = await response.Content.ReadFromJsonAsync<GetPredictionsResponseDTO>();
+			return responseBody.Predictions;
         }
     }
 }
